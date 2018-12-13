fb3a4eed1b4a960f8cc0e00172e0981651a7590d
Shubham Jain <shubham9@illinois.edu>
2018-12-12 18:09:30 -0600

    skeleton for article viewing

--- mysite/app1/views.py
+++ mysite/app1/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import auth
 #from django.core.context_processors import csrf
-from app1.models import Articles
+from app1.models import *
 from django.views.generic.base import TemplateView
 
 
@@ -31,10 +31,11 @@ def index(request):
 def articles(request):
 	if 'articleid' in request.GET:
 		message = 'You searched for: %r' % request.GET['articleid']
-	return  HttpResponse(message)
-	articledet = 0 # fetch article details
-	sourcedet = 0 # fetch source details
-	relatedpost = 0 #fetch related articles, their url, title, match%
+	# return  HttpResponse(message)
+	articledet = Articles.objects.filter(article_id=request.GET['articleid'])[0] # fetch article details
+	sourcedet = Author.objects.filter(source_id=articledet.source_id)[0] # fetch source details
+	relatedpost = 0
+	# relatedpost = list(ArticleSimilarity.objects.filter(name="bbc")) #fetch related articles, their url, title, match%
 	return render(request,'article.html',  {'article': articledet, 'relatedposts':relatedpost,'source':sourcedet})
 
 def news(request):
