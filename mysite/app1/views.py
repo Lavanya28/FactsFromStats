a7fe070365c92fa0dbb3e7468322724f63e185db
Lavanya Piramanayagam <lava281995@gmail.com>
2018-12-14 15:35:34 -0600

    removed

--- mysite/app1/views.py
+++ mysite/app1/views.py
@@ -61,12 +61,12 @@ def createarticles(request):
 	return render(request,'CreateArticle.html')
 
 def handlecreatearticles(request):
-	quiz = str(request.GET['quiz'])
+	
 	article = str(request.GET['article'])
 	title=str(request.GET['title'])
 	article_id_new = len(Articles.objects.all())
-	Articles.objects.create(source_id= 0 , link="https://www.thestreet.com/markets/flashback-friday-whats-the-market-smoking--14804612" + str(article_id_new), title = title +str(article_id_new), text=article, agreement_index=0, article_id = article_id_new , number_of_upvotes=0)
-	Quiz.objects.create(article_id=article_id_new, quiz = quiz)
+	Articles.objects.create(source_id= 0 , link="https://www.thestreet.com/markets/flashback-friday-whats-the-market-smoking--14804612" + str(article_id_new), title = title +str(article_id_new), text=article, agreement_index=5, article_id = article_id_new , number_of_upvotes=0)
+	
 
 	if not('pagenum' in request.GET):
 		return render(request,'news.html',  {'list_articles': processedlist[:10],'pagenum':0})
